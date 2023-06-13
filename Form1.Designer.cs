e1a91384b3f0b27650a11d8405ecd991ff544e94
Richard Poate <info@zoople.com.au>
2023-06-13 19:29:42 +1000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Zoom_Toolbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.htmlEditControl1 = new Zoople.HTMLEditControl();
+            this.SuspendLayout();
+            // 
+            // htmlEditControl1
+            // 
+            this.htmlEditControl1.AcceptsReturn = true;
+            this.htmlEditControl1.BaseURL = null;
+            this.htmlEditControl1.CleanMSWordHTMLOnPaste = true;
+            this.htmlEditControl1.CodeEditor.Enabled = true;
+            this.htmlEditControl1.CodeEditor.Locked = false;
+            this.htmlEditControl1.CodeEditor.TabWidth = 720;
+            this.htmlEditControl1.CodeEditor.WordWrap = false;
+            this.htmlEditControl1.CSSText = null;
+            this.htmlEditControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.htmlEditControl1.DocumentHTML = null;
+            this.htmlEditControl1.EnableInlineSpelling = false;
+            this.htmlEditControl1.FontSizesList = null;
+            this.htmlEditControl1.FontsList = null;
+            this.htmlEditControl1.HiddenButtons = null;
+            this.htmlEditControl1.ImageStorageLocation = null;
+            this.htmlEditControl1.InCodeView = false;
+            this.htmlEditControl1.IndentAmount = 2;
+            this.htmlEditControl1.IndentsUseBlockuote = false;
+            this.htmlEditControl1.LanguageFile = null;
+            this.htmlEditControl1.LicenceActivationKey = null;
+            this.htmlEditControl1.LicenceKey = null;
+            this.htmlEditControl1.LicenceKeyInlineSpelling = null;
+            this.htmlEditControl1.Location = new System.Drawing.Point(0, 0);
+            this.htmlEditControl1.Name = "htmlEditControl1";
+            this.htmlEditControl1.Size = new System.Drawing.Size(800, 450);
+            this.htmlEditControl1.TabIndex = 0;
+            this.htmlEditControl1.ToolstripImageScalingSize = new System.Drawing.Size(16, 16);
+            this.htmlEditControl1.UseParagraphAsDefault = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.htmlEditControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Zoople.HTMLEditControl htmlEditControl1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zoom_Toolbar
+{
+    public partial class Form1 : Form
+    {
+        readonly ToolStripComboBox oZoom;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            oZoom = new ToolStripComboBox
+            {
+                Padding = new Padding(3)
+            };
+            oZoom.Items.AddRange(new string[] { "200%", "100%", "75%", "50%" });
+            oZoom.SelectedIndexChanged += OZoom_TextUpdate;
+
+            this.htmlEditControl1.ToolStripItems.Add(oZoom);
+
+            this.htmlEditControl1.ZoomLevelChanged += HtmlEditControl1_ZoomLevelChanged;
+            this.htmlEditControl1.HideDOMToolbar = true;
+        }
+
+        private void OZoom_TextUpdate(object sender, EventArgs e)
+        {
+            this.htmlEditControl1.ZoomLevel = int.Parse(oZoom.Text.Replace("%", ""));
+            this.htmlEditControl1.SetFocus();
+        }
+
+        private void HtmlEditControl1_ZoomLevelChanged(object sender, EventArgs e)
+        {
+            oZoom.Text = this.htmlEditControl1.ZoomLevel.ToString() + "%";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
